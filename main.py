1f5d42d71144aeea79b3b3ce3058b0c4a928c373
Soheil <iamsoheil.r@gmail.com>
2024-05-11 22:13:11 +0330

    create food class and config game cells

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from pygame import *
+from pygame.math import Vector2
 import sys
+
+# Constants -> game colors
+GREEN = (173, 204, 96)
+DARK_GREEN = (43, 51, 24)
+
+# Cell config
+cell_size = 30
+number_of_cells = 25
 
 # Initializing pygame
 init()
 
+
+class Food:
+    def __init__(self):
+        self.position = Vector2(5, 6)
+
+    def draw(self):
+        food_rect = Rect(
+            self.position.x * cell_size,
+            self.position.y * cell_size,
+            cell_size,
+            cell_size,
+        )
+        draw.rect(surface=screen, color=DARK_GREEN, rect=food_rect)
+
+
 # Creating Canvas
-screen = display.set_mode((750, 750))
+screen = display.set_mode((cell_size * number_of_cells, cell_size * number_of_cells))
 display.set_caption("Retro Snake Game")
 clock = time.Clock()
+food = Food()
 
 # Game loop
 while True:
@@ -15,6 +40,9 @@ while True:
         if events.type == QUIT:
             quit()
             sys.exit()
+
+    screen.fill(GREEN)
+    food.draw()
 
     display.update()
     # Controlling frame rate (60)
